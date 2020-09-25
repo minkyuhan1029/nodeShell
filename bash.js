946e2da09ea24450330be80c4739f1ac02c0ddbd
Min Kyu Han <minkyu.han1029@gmail.com>
2020-09-25 15:28:17 -0400

    Added filtering

--- bash.js
+++ bash.js
@@ -6,8 +6,14 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    pwd();
-    ls();
+    // pwd();
+    // ls();
     process.stdout.write('You typed: ' + cmd);
+    if(cmd === 'pwd'){
+        pwd();
+    }
+    if(cmd === 'ls'){
+        ls();
+    }
     process.stdout.write('\nprompt > ');
 })
